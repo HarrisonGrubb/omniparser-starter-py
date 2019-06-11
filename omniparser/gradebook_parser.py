739b82fc427bb4589f6b80bee8c64ae95fe74f54
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-10 20:59:42 -0400

    added list comprehension

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -5,7 +5,9 @@ def calculate_average_grade_from_csv(my_csv_filepath):
     dataframe = pd.read_csv(my_csv_filepath)
     avg = dataframe['final_grade'].mean()
     return avg
-
+    #list comprehension follow along
+    # rows = df.to_dict(dataframe)
+    # grades = [r['final_grade] for r in rows]
 
 if __name__ == "__main__":
     print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
