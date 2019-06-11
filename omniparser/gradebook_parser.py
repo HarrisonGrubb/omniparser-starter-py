09769b9733573302b595204f82016daeb185472b
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-10 20:54:23 -0400

    gradebook test pass

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -1,2 +1,15 @@
+import pandas as pd 
+import os
 
-print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+def calculate_average_grade_from_csv(my_csv_filepath):
+    dataframe = pd.read_csv(my_csv_filepath)
+    avg = dataframe['final_grade'].mean()
+    return avg
+
+
+if __name__ == "__main__":
+    print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv")
+
+    calculate_average_grade_from_csv(gradebook_filepath)
