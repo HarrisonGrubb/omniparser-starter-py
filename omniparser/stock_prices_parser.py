fc454a633e2a2b1425efad3f37581e76e37317e4
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-12 20:59:22 -0400

    working on stocks

--- omniparser/stock_prices_parser.py
+++ omniparser/stock_prices_parser.py
@@ -1,2 +1,17 @@
+import json
+import os
+import pandas as pd 
+import statistics
 
-print("PARSING SOME STOCK PRICES HERE...")
+def calculate_latest_closing_price_from_json(location):
+    with open(location, 'r') as reader:
+        contents = reader.read()
+        #print(contents)
+
+    data = json.loads(contents)
+    daily_data = data["Time Series (Daily)"]
+    #need to get last post
+if __name__ == "__main__":
+    
+    path = os.path.join(os.path.dirname(__file__), "..", "data", "stock_prices_aapl.json")
+    calculate_latest_closing_price_from_json(path)
