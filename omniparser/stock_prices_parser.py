1d1b821136c480b1dc95f6f7b030c5947c9e1bda
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-13 21:19:55 -0400

    pandas fix

--- omniparser/stock_prices_parser.py
+++ omniparser/stock_prices_parser.py
@@ -2,6 +2,7 @@ import json
 import os
 import pandas as pd 
 import statistics
+from pandas.io.json import json_normalize
 
 def calculate_latest_closing_price_from_json(location):
     with open(location, 'r') as reader:
@@ -10,7 +11,11 @@ def calculate_latest_closing_price_from_json(location):
 
     data = json.loads(contents)
     daily_data = data["Time Series (Daily)"]
-    #need to get last post
+    weird_df = pd.DataFrame(daily_data)
+    dataframe = weird_df.T
+    print(dataframe)
+
+    
 if __name__ == "__main__":
     
     path = os.path.join(os.path.dirname(__file__), "..", "data", "stock_prices_aapl.json")
